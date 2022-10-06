42274f74c2b9afa4c29d8064e1944c0bae3230b0
DanielWilches <danielprietowilches@gmail.com>
2022-10-05 19:15:33 -0500

    primera confirmacion

---
+++ o_de_solid/S_de_Solid/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_de_Solid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ o_de_solid/o_de_solid/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o_de_solid
+{
+
+    public class Orden 
+    {
+        private string linelItems = string.Empty;
+        private IShipping shipping;
+        public double getTotal() 
+        {
+            return 0.0;
+        }
+        public void getTotalWeight() { }
+        public void setShippingType(IShipping shpiiping) { }
+        public string getShippingCost() { return string.Empty; }
+        public void getShippingDate(string shippingCost) { }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Air : IShipping
+    {
+        public object getCost(object Orden) 
+        {
+            return null;
+        }
+        public object getDate(object Orden) 
+        {
+            return null;
+        }
+    }
+
+    public class Train : IShipping
+    {
+        public object getCost(object Orden)
+        {
+            return null;
+        }
+        public object getDate(object Orden)
+        {
+            return null;
+        }
+    }
+    public class Water : IShipping
+    {
+        public object getCost(object Orden)
+        {
+            return null;
+        }
+        public object getDate(object Orden)
+        {
+            return null;
+        }
+    }
+
+
+
+    public interface IShipping 
+    {
+        object getCost(object Orden);
+        object getDate(object Orden);
+    
+    }
+}
